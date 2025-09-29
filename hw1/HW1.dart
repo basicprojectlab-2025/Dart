988176b4a306875389cd5fee8a8fd6be67224211
donghyeon choi <downpool.choi@gmail.com>
2025-09-29 16:31:47 +0900

    hw1

---
+++ hw1/HW1.dart
@@ -0,0 +1,13 @@
+void main(List<String> arg) {
+  int ans = 0;
+  int argi = int.parse(arg[0]);
+  if (argi < 0) {
+    print(argi);
+  } else {
+    while (argi >= 1) {
+      ans += argi % 10;
+      argi = argi ~/ 10;
+    }
+  }
+  print(ans);
+}
